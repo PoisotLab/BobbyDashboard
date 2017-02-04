618c02e41fb77c3f6ea72b34074073456eaf4ea0
Timothée Poisot <tim@poisotlab.io>
2017-02-04 10:23:40 -0500

    get the observations for a project using the iNat API

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var request = require('request');
 var config = require('config');
 
 // Load configuration
-eventConfig = config.get('event');
+confEvent = config.get('event');
+confINat = config.get('iNaturalist');
 
-console.log(eventConfig.slug);
+// URL for project observations
+var projectObsURL = confINat.api + 'observations/project/' + confEvent.slug + '.json';
+
+// Callback function for project observations
+function observation_callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var project_observations = JSON.parse(body)
+        console.log(project_observations)
+    }
+}
+
+// Get the list of observations
+request(projectObsURL, observation_callback)
+
