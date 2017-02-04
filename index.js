2a01e7bb1b17caf1ea9a216a635349cb4a8cbc88
Timothée Poisot <tim@poisotlab.io>
2017-02-04 18:08:24 -0500

    pages through inaturalist

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Load the required modules
 var rp = require('request-promise')
 var config = require('config')
+var fs = require('fs')
 
 // Web packages
 var express = require('express')
@@ -25,8 +26,24 @@ dashboard.use(stylus({
 
 dashboard.use(express.static(publicDir))
 
-// Build URL for project observations
-var projectObsURL = 'http://www.inaturalist.org/' + 'observations/project/' + confEvent.slug + '.json'
+// Caching the results, updated every five minutes or unless called explicitely
+function readEventPage(slug, per_page, page) {
+    console.log("Reading event stream")
+    // Build the URL
+    var url = 'http://www.inaturalist.org/' + 
+        'observations/project/' + slug + '.json' +
+        '?page=' + page + '&per_page=' + per_page
+    // Get the URL
+    rp(url)
+    .then(function(result) {
+        console.log("Got results")
+        var obs = JSON.parse(result)
+        res.send(obs)
+
+    })
+    .catch(generic_error)
+}
+
 
 // Generic callbacks
 function generic_error(err) {
@@ -37,8 +54,11 @@ dashboard.get('/map', function (req, res) {
     res.render('map')
 })
 
-dashboard.get('/obs', function (req, res) {
+dashboard.get('/obs/:page', function (req, res) {
+    // Build URL for project observations
+    var projectObsURL = 'http://www.inaturalist.org/' + 'observations/project/' + confEvent.slug + '.json?page=' + req.params.page
     console.log('Getting request to update the observations')
+    console.log(projectObsURL)
     rp(projectObsURL)
     .then(function (result) {
         var observations = JSON.parse(result)
